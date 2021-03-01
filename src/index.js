c0fd3a8f36a602566c0f725dc4c0c49e475a72bd
Jahaira-Marrero <jahaira.marrero@gmail.com>
2021-03-01 12:02:26 -0500

    Created new toy form.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,54 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function renderToys(){
+  fetch(`http://localhost:3000/toys`)
+  .then(response => response.json())
+  .then(toysArray => {
+    toysArray.forEach(toy => {
+      renderToy(toy)
+    })
+  })
+}
+
+function renderToy(toy) {
+  const div = document.createElement('div')
+  div.classList.add('card')
+  div.innerHTML = `<h2>${toy.name}</h2>
+    <img src= ${toy.image} class="toy-avatar"> 
+    <p>${toy.likes} likes</p>
+    <button class="like-btn"> Like <3 </button>`
+
+  const toys = document.querySelector('div#toy-collection')
+  toys.append(div)
+}
+
+const form = document.querySelector('form.add-toy-form')
+form.addEventListener("submit", function(event) {
+  event.preventDefault()
+  const name = event.target[0].value
+  const image = event.target[1].value
+  const toy = {
+    name: name, 
+    image: image,
+    likes: 0
+  }
+  fetch(`http://localhost:3000/toys`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(toy),
+  })
+    .then(response => response.json())
+    .then(oneToy => { renderToy(oneToy)})
+    .catch((error) => {console.error("Error:", error);})
+
+  event.target.reset()
+})
+
+const addLikes = document.querySelector("")
+
+renderToys() 
